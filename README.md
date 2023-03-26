aaea883ffebe4358a6793516047445154c592f9c
Ester N  Freitas <101005237+TherNF@users.noreply.github.com>
2023-03-26 13:22:50 -0300

    final

---
+++ README.md
@@ -0,0 +1,13 @@
+**Desafio Bootcamp Orange Tech + | BackEnd**
+
+<u>Abstração, Encapsulamento, Herança e Polimorfismo.</u> 
+
+
+
+Passo-a-Passo utilizado:
+Abstração do domínio bootcamp e modelagem de seus atributos e métodos;
+Criação das classes: bootcamp, cursos, mentorias e devs e seus respectivos relacionamentos;
+Modelagem das classes curso, mentoria e devs também criação de seus atributos e métodos;
+Utilização dos paradigmas de orientação a objetos (poo): abstração, encapsulamento, herança e polimorfismo para que o código ficasse legível e de fácil manutenção;
+Criação de objetos para representar classes que foram criadas e relacionadas.
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+
 
 import java.time.LocalDate;
 
@@ -20,13 +23,36 @@ public class Main {
         mentoria.setDescricao(" descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcampo Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devEster= new Dev();
+        devEster.setNome("Ester");
+        devEster.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos :" + devEster.getConteudosInscritos());
+        devEster.progredir();
+        devEster.progredir();
+        devEster.progredir();
+        System.out.println("-");
+        System.out.println(" Conteúdos Inscritos Ester :" + devEster.getConteudosInscritos());
+        System.out.println(" Conteúdos Concluidos Ester :" + devEster.getConteudosConcluidos());
+        System.out.println("XP:" + devEster.calcularTotalXp());
 
+        System.out.println("------");
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev devFernando= new Dev();
+        devEster.setNome("Fernando");
+        devFernando.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos :" + devFernando.getConteudosInscritos());
+        devFernando.progredir();
+        System.out.println("-");
+        System.out.println(" Conteúdos Inscritos Fernando:" + devFernando.getConteudosInscritos());
+        System.out.println(" Conteúdos Concluidos Fernando:" + devFernando.getConteudosConcluidos());
+        System.out.println("XP:" + devFernando.calcularTotalXp());
 
-
-    }
+   }
 }
